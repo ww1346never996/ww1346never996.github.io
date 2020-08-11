25fa2e13647debc0236195444c52b41fcf33cc43
ww1346never996 <wmr1346@bupt.edu.cn>
2020-08-12 00:30:20 +0800

    Site updated: 2020-08-12 00:30:17

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+	  {
+		name: 'Gravity',
+		artist: 'Novelist FR',
+		url: 'https://music.163.com/song/media/outer/url?id=1458474926.mp3'
+	}
+    ]
+});
+
