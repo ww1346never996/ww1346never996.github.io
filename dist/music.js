fc66cf316e2a258e5cdb9d292c4ecc938e134d2d
ww1346never996 <wmr1346@bupt.edu.cn>
2020-08-12 15:35:21 +0800

    Site updated: 2020-08-12 15:35:18

--- dist/music.js
+++ dist/music.js
@@ -7,6 +7,11 @@ const ap = new APlayer({
 		name: 'Gravity',
 		artist: 'Novelist FR',
 		url: 'https://music.163.com/song/media/outer/url?id=1458474926.mp3'
+	},
+	{
+		name: 'LEAVES',
+		artist: 'Earthists.',
+		url: 'https://music.163.com/song/media/outer/url?id=1298841854.mp3'
 	}
     ]
 });
